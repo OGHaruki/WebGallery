d777ec3f92a9fecf5a4b587f0473ee550c947d32
OGHaruki <jajestemadam@gmail.com>
2023-08-23 23:38:51 +0200

    securityFilterChain and ApplicationConfig updated.

--- src/main/java/com/myprojects/webgallery/config/SecurityConfig.java
+++ src/main/java/com/myprojects/webgallery/config/SecurityConfig.java
@@ -1,64 +1,46 @@
 package com.myprojects.webgallery.config;
 
-import com.myprojects.webgallery.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
 
-    private UserService userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers(new AntPathRequestMatcher("/signup/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
-                                .requestMatchers(new AntPathRequestMatcher("/admin/**"))
-                                .hasRole("ADMIN")
-                                .anyRequest().authenticated()
-                /*)
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .successHandler((request, response, authentication) -> {
-                            for (GrantedAuthority auth : authentication.getAuthorities()) {
-                                if (auth.getAuthority().equals("ADMIN")) {
-                                    response.sendRedirect("/image/upload");
-                                    return;
-                                }
-                            }
-                            response.sendRedirect("/");
-                        })
-                        .permitAll()*/
-                );
+                                authorize.requestMatchers(new AntPathRequestMatcher("/signup/**")).permitAll()
+                                        .requestMatchers(new AntPathRequestMatcher("/login/**")).permitAll()
+                                        .requestMatchers(new AntPathRequestMatcher("/admin/**"))
+                                        .hasRole("ADMIN")
+                                        .anyRequest().authenticated()
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
 
         return http.build();
     }
